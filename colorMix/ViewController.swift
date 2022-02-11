03eb8f51e8d68dd24eb8aa02013e2e6fbd0edda1
Shapovalov_Ilay <greateman@mail.ru>
2022-02-11 20:15:48 +0300

    colorMix app 1.0. My 1-st UIKit app

--- colorMix/ViewController.swift
+++ colorMix/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    func updateControls() {
+        redSlider.isEnabled = redSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor = UIColor.black.cgColor
+        updateControls()
+        updateColor()
     }
-
-
+    
+    @IBOutlet weak var colorView: UIView!
+        
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateControls()
+        updateColor()
+        }
+    
+    @IBAction func sladerChanged(_ sender: Any) {
+        updateColor()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        redSwitch.isOn = false
+        redSlider.value = 1
+        greenSwitch.isOn = false
+        greenSlider.value = 1
+        blueSwitch.isOn = false
+        blueSlider.value = 1
+        updateControls()
+        updateColor()
+    }
+    
 }
+
 
